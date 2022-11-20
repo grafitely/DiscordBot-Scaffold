b517a6d477406c85c2cc205fe5390e094dac1405
grafitely <88990666+grafitely@users.noreply.github.com>
2022-11-20 22:28:46 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+// Imports libraries, dotenv(https://www.npmjs.com/package/dotenv), discord.js(https://discord.js.org/#/)
+const { Client, GatewayIntentBits, Events, REST, Routes, CommandInteraction, } = require("discord.js")
+require("dotenv/config")
+
+//initiates client with Intents(https://discordjs.guide/popular-topics/intents.html#privileged-intents)
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+})
+
+//communicates with discords api to help initialize slash commands (https://discordjs.guide/additional-info/rest-api.html#making-http-requests-with-node)
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+//states the bot is initialized
+client.on("ready", () => {
+    console.log("DxD >>>>")  
+})
+
+//example event of the bot being annoying and reading out a deleted message
+client.on(Events.MessageDelete, message => {
+    const replymessage = "<@" + message.author + "> Just deleted a message!!! it was '" + message.content + "'";
+    message.channel.send(replymessage);
+})
+
+//initializes slash commands 
+async function main (){
+    try{
+        const commands = [
+            {
+              name: 'ping',
+              description: 'Replies with Pong!',
+            },
+        ];
+
+        await rest.put(Routes.applicationCommands(process.env.CLIENTID), {
+            body: commands
+        });
+    }
+    catch(e){   
+
+    } 
+}
+
+//makes slash commands actually do something
+client.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isChatInputCommand()){
+        return;
+    }
+
+    if(interaction.commandName === "ping"){
+        interaction.reply("pong");
+    }
+})
+
+//double checks that the bot isnt replying to itself when spamming balls
+client.on(Events.MessageCreate, message => {
+    if(message.author == client.user || message.author == message.author.bot){
+       return false; 
+    }
+    for(let i = 0; i < 10; i++) {
+        setTimeout(() => { message.reply("balls") }, 5000);
+    }
+})
+
+main();
+
+//logs on the bot
+client.login(process.env.TOKEN);
